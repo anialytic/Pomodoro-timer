ed567a0013c1367e326b3d3c7789848bcce5ccb5
Ania <anna.kors.19.1@gmail.com>
2026-02-07 20:36:25 +0200

    Add break

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-let time = 25 * 60; // 25 хвилин у секундах
+let workTime = 24 * 60;
+let breakTime = 5 * 60;
+
+let time = workTime;
+let mode = "work"; // work | break
+
 let timerInterval = null;
 
 const timerDisplay = document.getElementById("timer");
@@ -8,40 +13,53 @@ const resetBtn = document.getElementById("reset");
 
 // Форматування часу
 function updateTimer() {
-    let minutes = Math.floor(time / 60);
-    let seconds = time % 60;
+  let minutes = Math.floor(time / 60);
+  let seconds = time % 60;
 
-    timerDisplay.textContent =
-        `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+  timerDisplay.textContent =
+    `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
 }
 
 // Старт
 startBtn.addEventListener("click", () => {
-    if (timerInterval !== null) return;
+  if (timerInterval !== null) return;
 
-    timerInterval = setInterval(() => {
-        time--;
-        updateTimer();
+  timerInterval = setInterval(() => {
+    time--;
+    updateTimer();
 
-        if (time <= 0) {
-            clearInterval(timerInterval);
-            alert("Time is up!");
-        }
-    }, 1000);
+    if (time <= 0) {
+      clearInterval(timerInterval);
+      timerInterval = null;
+
+      if (mode === "work") {
+        mode = "break";
+        time = breakTime;
+        alert("Час на перерву ☕");
+      } else {
+        mode = "work";
+        time = workTime;
+        alert("Повертаємось до роботи 💪");
+      }
+
+      updateTimer();
+    }
+  }, 1000);
 });
 
 // Пауза
 pauseBtn.addEventListener("click", () => {
-    clearInterval(timerInterval);
-    timerInterval = null;
+  clearInterval(timerInterval);
+  timerInterval = null;
 });
 
 // Reset
 resetBtn.addEventListener("click", () => {
-    clearInterval(timerInterval);
-    timerInterval = null;
-    time = 25 * 60;
-    updateTimer();
+  clearInterval(timerInterval);
+  timerInterval = null;
+  mode = "work";
+  time = workTime;
+  updateTimer();
 });
 
 // Перший рендер
